63d1dabfb9a2cfd26cf4283749aaa1c39fbda156
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 21:18:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,9 @@ def roll_call_dwarves(dwarf_list)
 end
 
 def summon_captain_planet(planeteer_calls)
-  
+  planeteer_calls.collect |i|
+  i.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
